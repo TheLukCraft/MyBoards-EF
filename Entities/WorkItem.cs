88d46b458c94eb96389d46d9b977ed437a4285c3
TheLukCraft <luk1997r@o2.pl>
2024-11-16 17:47:23 +0100

    Configuring 1:n relation

--- Entities/WorkItem.cs
+++ Entities/WorkItem.cs
@@ -19,4 +19,7 @@ public class WorkItem
     public decimal RemaningWork { get; set; }
 
     public string? Type { get; set; }
+    public List<Comment>? Comments { get; set; } = new List<Comment>();
+    public User? Author { get; set; }
+    public Guid AuthorId { get; set; }
 }
