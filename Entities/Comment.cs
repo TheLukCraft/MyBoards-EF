67fd79f37dd019d582f922ff94c1251b40ac7c6e
TheLukCraft <luk1997r@o2.pl>
2024-11-16 16:36:33 +0100

    Primary key

--- Entities/Comment.cs
+++ Entities/Comment.cs
@@ -4,6 +4,7 @@ namespace MyBoards.Entities;
 
 public class Comment
 {
+    public int Id { get; set; }
     public string Message { get; set; }
     public string Author { get; set; }
     public DateTime CreatedDate { get; set; }
--- Entities/Tag.cs
+++ Entities/Tag.cs
@@ -4,5 +4,6 @@ namespace MyBoards.Entities;
 
 public class Tag
 {
+    public int Id { get; set; }
     public string Value { get; set; }
 }
